9ce555cf497c0d8ab685519bc1862fa5fba3785e
HCricle <15218450198@163.com>
2024-11-30 20:31:54 +0800

    Update assembly name & fix ci

--- uploadnuget.ps1
+++ uploadnuget.ps1
@@ -4,6 +4,6 @@ dotnet build -c Release DataRepository.sln
 $paths=ls src | select Name
 for($x=0;$x -lt $paths.length; $x=$x+1)
 {
-$fp=-join ("src\",$paths[$x].Name,"\bin\Release\",$paths[$x].Name,".",$version,".nupkg");
+$fp=-join ("src\",$paths[$x].Name,"\bin\Release\","D",$paths[$x].Name,".",$version,".nupkg");
 dotnet nuget push $fp -k $accessToken -s https://api.nuget.org/v3/index.json
 }
