940ea4e236052d346a6e71fa3be2715144538e0f
Cricle <15218450198@163.com>
2024-12-27 07:28:24 +0800

    Update uploadnuget.ps1

--- uploadnuget.ps1
+++ uploadnuget.ps1
@@ -1,9 +1,9 @@
 $accessToken=$args[0]
 $version=Get-Content version
 dotnet build -c Release DataRepository.sln -p:Version=$version
-$paths=ls src | select Name
+$paths=ls -Directory src | select Name
 for($x=0;$x -lt $paths.length; $x=$x+1)
 {
 $fp=-join ("src\",$paths[$x].Name,"\bin\Release\","D",$paths[$x].Name,".",$version,".nupkg");
-dotnet nuget push $fp -k $accessToken -s https://api.nuget.org/v3/index.json
+dotnet nuget push $fp -k $accessToken -s https://api.nuget.org/v3/index.json --skip-duplicate
 }
