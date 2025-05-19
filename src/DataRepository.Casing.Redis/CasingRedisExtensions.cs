fd299f553e68338c6e20575cdf088592523df688
HCricle <15218450198@163.com>
2025-05-19 23:08:27 +0800

    Support aot

--- src/DataRepository.Casing.Redis/CasingRedisExtensions.cs
+++ src/DataRepository.Casing.Redis/CasingRedisExtensions.cs
@@ -2,6 +2,7 @@
 using DataRepository.Casing.Redis;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using System.Text.Json;
+using DataRepository.Casing.Models;
 #if !NETSTANDARD
 using System.Diagnostics.CodeAnalysis;
 #endif
@@ -10,6 +11,24 @@ namespace Microsoft.Extensions.DependencyInjection
 {
     public static class CasingRedisExtensions
     {
+        public static IServiceCollection AddRedisCasting<
+#if !NETSTANDARD
+            [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.All)]
+#endif
+        TEntity,
+#if !NETSTANDARD
+            [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.All)]
+#endif
+        TPublish>(this IServiceCollection services, JsonSerializerOptions? jsonSerializerOptions = null)
+            where TPublish : class, IValuePublisher<TEntity>
+            where TEntity : ITimedValue
+        {
+            services.TryAddSingleton<IValuePublisher<TEntity>, TPublish>();
+            services.TryAddSingleton<ICasingNewest<TEntity>, RedisHashCasingNewest<TEntity>>();
+            services.TryAddSingleton<INewestValueConverter<TEntity>, DIJsonNewestValueConverter<TEntity>>();
+            services.TryAddSingleton(new JsonOptionsBox(jsonSerializerOptions ?? JsonSerializerOptions.Default));
+            return services;
+        }
         public static IServiceCollection AddRedisCasting(this IServiceCollection services, Type valuePublisherType, JsonSerializerOptions? jsonSerializerOptions = null)
         {
             services.TryAdd(ServiceDescriptor.Describe(typeof(IValuePublisher<>), valuePublisherType, ServiceLifetime.Singleton));
@@ -19,10 +38,34 @@ namespace Microsoft.Extensions.DependencyInjection
             return services;
         }
 
-        public static IServiceCollection AddRedisNewest(this IServiceCollection services,RedisHashCasingNewestConfig newestConfig)
+        public static IServiceCollection AddRedisNewest<
+#if !NETSTANDARD
+            [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.All)]
+#endif
+        TEntity>(this IServiceCollection services, RedisHashCasingNewestConfig newestConfig)
+            where TEntity : ITimedValue
+        {
+            services.TryAddSingleton<ICasingNewest<TEntity>,RedisHashCasingNewest<TEntity>>();
+            services.TryAddSingleton(newestConfig);
+            return services;
+        }
+
+        public static IServiceCollection AddRedisNewest(this IServiceCollection services, RedisHashCasingNewestConfig newestConfig)
         {
             services.TryAdd(ServiceDescriptor.Describe(typeof(ICasingNewest<>), typeof(RedisHashCasingNewest<>), ServiceLifetime.Singleton));
             services.TryAddSingleton(newestConfig);
+            return services;
+        }
+
+        public static IServiceCollection AddRedisTopN<
+#if !NETSTANDARD
+            [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.All)]
+#endif
+        TEntity>(this IServiceCollection services, RedisSortSetTopNConfig topNConfig)
+            where TEntity : ITimedValue
+        {
+            services.TryAddSingleton<ITopN<TEntity>,RedisSortSetTopN<TEntity>>();
+            services.TryAddSingleton(topNConfig);
             return services;
         }
 
@@ -49,7 +92,7 @@ namespace Microsoft.Extensions.DependencyInjection
 #endif
         TModel> : JsonNewestValueConverter<TModel>
         {
-            public DIJsonNewestValueConverter(JsonOptionsBox box) 
+            public DIJsonNewestValueConverter(JsonOptionsBox box)
                 : base(box.JsonSerializerOptions)
             {
             }
